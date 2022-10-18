5c8c3b6896ce56522cbe8e928e57414d5a0e926b
Mario <A01732529@tec.mx>
2022-10-18 12:10:45 -0500

    First commit

---
+++ red_blur.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define OMP_NUM_THREADS 10
+#define HEADER_SIZE 138
+
+const int valid_dimensions[] = {9, 15, 23, 29, 35, 41};
+
+int main (int argc, char *argv[])
+{
+  //char *img = 
+  return 0;
+}
