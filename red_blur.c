5bb3c70a2d0b320b8d89d4db620d0fb692509faa
Mario <A01732529@tec.mx>
2022-10-18 12:55:41 -0500

    Second program version

--- red_blur.c
+++ red_blur.c
@@ -10,9 +10,10 @@ const int valid_dimensions[] = {9, 15, 23, 29, 35, 41};
 int main (int argc, char *argv[])
 {
   char img[] = "f1.bmp";
-  FILE *ip = fopen(img, "rb");
+  FILE *ip = fopen(img, "rb+");
   unsigned char header[HEADER_SIZE];
   int width = 0, height = 0, isValid = 0, redMean = 0;
+  int kernel = 9;
 
   if (ip != NULL) {
     for (int i = 0; i < HEADER_SIZE; i++) {
@@ -22,16 +23,13 @@ int main (int argc, char *argv[])
     width = (int)header[20] * 65536 + (int)header[19] * 256 + (int)header[18];
     height = (int)header[24] * 65536 + (int)header[23] * 256+ (int)header[22];
 
-    printf("width: %d\n", width);
-    printf("height: %d\n", height);
-
-    //#pragma omp parallel for
+    #pragma omp parallel for
     for (int i = 0; i < 6; i++) {
-      if (width == valid_dimensions[i] && height == valid_dimensions[i]) {
-        //#pragma single 
-        //{
+      if (kernel == valid_dimensions[i]) {
+        #pragma single 
+        {
           isValid = 1;
-        //}
+        }
       }
     }
 
@@ -43,12 +41,13 @@ int main (int argc, char *argv[])
     fseek(ip, HEADER_SIZE, SEEK_SET);
 
     for (int i = 0; i < height; i++) {
-        // 2 equals blurring factor
-      for (int j = 0; j < width; j++) {
+      redMean = 0;
+      for (int j = 0; j < width + kernel; j++) {
         redMean += (int)fgetc(ip);
         fgetc(ip);
         fgetc(ip);
       }
+      fseek(ip, -3 * (width + kernel), SEEK_CUR);
     }
 
     redMean = redMean / width;
@@ -56,9 +55,10 @@ int main (int argc, char *argv[])
     fseek(ip, HEADER_SIZE, SEEK_SET);
 
     for (int i = 0; i < height; i++) {
-        // 2 equals blurring factor
       for (int j = 0; j < width; j++) {
         fputc(redMean, ip);
+        fputc(fgetc(ip), ip);
+        fputc(fgetc(ip), ip);
       }
     }
 
